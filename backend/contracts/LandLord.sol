d451725dfed4de9cf785f1a92ddee2ecc87ef4a9
Josef <josef.dabrowski@gmail.com>
2022-05-01 10:29:40 +0800

    Keeper compatibility - checkUpkeep, performUpkeep

--- backend/contracts/LandLord.sol
+++ backend/contracts/LandLord.sol
@@ -3,13 +3,14 @@
 pragma solidity >=0.6.0 <0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
 
 // 1. Accept payment from tenant to landlord (payment screen)
 // 2. Write public view function to get balance of the contract (chainlink keeper)
 // 3. return a bool if youre the landlord (landlord dashboard page)
 // 4. Required: X amount of tenants
 
-contract LandLord is Ownable{
+contract LandLord is Ownable, KeeperCompatibleInterface {
 
     mapping(address => bool) public my_tenants;
     address payable[] public tenants;
@@ -34,4 +35,46 @@ contract LandLord is Ownable{
     function isOwner() public view virtual returns (bool) {
         return msg.sender == owner();
     }
+
+    function checkUpkeep(bytes calldata /* checkData */) external view override returns (bool upkeepNeeded, bytes memory /* performData */) {
+        upkeepNeeded = address(this).balance > 2 ether; //2 * 1000000000000000000;
+        performData = abi.encode(address(this));
+        return (upkeepNeeded, performData);
+    }
+
+    function performUpkeep(bytes calldata /* performData */) external override {
+
+        if ((address(this).balance > 3 ether ) {
+            //transfer to aave
+            //
+        }
+    }
+
+      /**
+   * @notice Get list of addresses that are underfunded and return keeper-compatible payload
+   * @return upkeepNeeded signals if upkeep is needed, performData is an abi encoded list of addresses that need funds
+   */
+   function checkUpkeep(bytes calldata)
+    external
+    view
+    override
+    whenNotPaused
+    returns (bool upkeepNeeded, bytes memory performData)
+  {
+    address[] memory needsFunding = getUnderfundedAddresses();
+    upkeepNeeded = needsFunding.length > 0;
+    performData = abi.encode(needsFunding);
+    return (upkeepNeeded, performData);
+  }
+
+  /**
+   * @notice Called by keeper to send funds to underfunded addresses
+   * @param performData The abi encoded list of addresses to fund
+   */
+  function performUpkeep(bytes calldata performData) external override onlyKeeperRegistry whenNotPaused {
+    address[] memory needsFunding = abi.decode(performData, (address[]));
+    topUp(needsFunding);
+  }
+
+
 }
