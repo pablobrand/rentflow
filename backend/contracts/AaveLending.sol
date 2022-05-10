034987b8f905e7d92fa90e1fc03503aeccf20af7
kmcmearty <kmcmeart@gmail.com>
2022-05-10 07:29:21 -0400

    Push aave v3 pool non working code

--- backend/contracts/AaveLending.sol
+++ backend/contracts/AaveLending.sol
@@ -1,66 +1,60 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.10;
+// // SPDX-License-Identifier: MIT
+// pragma solidity 0.8.10;
 
-import { IPoolAddressesProvider } from '@aave/contracts/interfaces/IPoolAddressesProvider.sol';
+// import { IPoolAddressesProvider } from '@aave/contracts/interfaces/IPoolAddressesProvider.sol';
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { IPool } from "@aave/contracts/interfaces/IPool.sol";
-import "./ILendingSolution.sol";
-//import "@aave/contracts/interfaces/aave/IPool.sol";
-//import "@aave/contracts/interfaces/IPool.sol";
-//import "@aave/contracts/misc/AaveProtocolDataProvider.sol";
+// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+// import { IPool } from "@aave/contracts/interfaces/IPool.sol";
+// import "./ILendingSolution.sol";
+// //import "@aave/contracts/interfaces/aave/IPool.sol";
+// //import "@aave/contracts/interfaces/IPool.sol";
+// //import "@aave/contracts/misc/AaveProtocolDataProvider.sol";
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
+// // import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract AaveLending is ILendingSolution {
+// contract AaveLending is ILendingSolution {
 
-    // Openzeppelin tokens
-    IERC20 public aToken;
-    IERC20 public tokenUsedForPayments;
+//     // Openzeppelin tokens
+//     IERC20 public aToken;
+//     IERC20 public tokenUsedForPayments;
 
-    // aave pool
-    IPoolAddressesProvider internal addressProvider = IPoolAddressesProvider(0x651b8A8cA545b251a8f49B57D5838Da0a8DFbEF9);
-    IPool internal aavePool = IPool(addressProvider.getPool());
-
-    // ILendingPool internal aaveLendingPool = ILendingPool(0xE0fBa4Fc209b4948668006B2bE61711b7f465bAe);
-    //provider = addressProvider
-    //TokenData[] aTokens = getAllATokens()
-    //address public aETHTokenAddress = 0xE101EcB2283Acf0C91e05A428DDD8833Ac66B572;
+//     // aave pool
+//     //IPoolAddressesProvider internal addressProvider = IPoolAddressesProvider(0x651b8A8cA545b251a8f49B57D5838Da0a8DFbEF9);
+//     IPool internal aavePool;
     
-    // contract variables
+//     // addresses
+//     address public poolAddressesProvider;
+//     address public aETHTokenAddress = 0xec6E5B3Bd3e8CC74756Af812994361d8D1EF30F8;
     
-    // addresses
-    address public poolAddressesProvider;
-    address public aETHTokenAddress = 0xec6E5B3Bd3e8CC74756Af812994361d8D1EF30F8;
-    
-    address payable public owner;
-    uint256 public depositedAmountBalance;
+//     address payable public owner;
+//     uint256 public depositedAmountBalance;
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Restricted to the owner only");
-        _;
-    }
+//     modifier onlyOwner() {
+//         require(msg.sender == owner, "Restricted to the owner only");
+//         _;
+//     }
 
-    constructor(address _tokenUsedToPay) {
-        tokenUsedForPayments = IERC20(_tokenUsedToPay);
-        aToken = IERC20(aETHTokenAddress);
-        owner = payable(msg.sender);
-    }
+//     constructor(address _tokenUsedToPay, address _aavePoolAddress) {
+//         tokenUsedForPayments = IERC20(_tokenUsedToPay);
+//         aToken = IERC20(aETHTokenAddress);
+//         aavePool = IPool(_aavePoolAddress);
+//         owner = payable(msg.sender);
+//     }
 
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0), "new owner is the zero address");
-        owner = payable(newOwner);
-    }
+//     function transferOwnership(address newOwner) public onlyOwner {
+//         require(newOwner != address(0), "new owner is the zero address");
+//         owner = payable(newOwner);
+//     }
 
-    function depositFunds(uint256 amount) external override(ILendingSolution) onlyOwner {
-        // require(amount <= tokenUsedForPayments.balanceOf(address(this)), "amount exceeds contract balance");
-        // Approve the LendingPool contract to pull the amount to deposit
-        tokenUsedForPayments.approve(address(aavePool), amount);
-        // Deposit the amount in the LendingPool
-        aavePool.deposit(address(tokenUsedForPayments), amount, address(this), 0);
+//     function depositFunds(uint256 amount) external override(ILendingSolution) {
+//         // require(amount <= tokenUsedForPayments.balanceOf(address(this)), "amount exceeds contract balance");
+//         // Approve the LendingPool contract to pull the amount to deposit
+//         tokenUsedForPayments.approve(address(aavePool), amount);
+//         // Deposit the amount in the LendingPool
+//         aavePool.deposit(address(tokenUsedForPayments), amount, address(this), 0);
 
-        depositedAmountBalance += amount;
-    }
-}
+//         // depositedAmountBalance += amount;
+//     }
+// }
--- backend/contracts/LandLord.sol
+++ backend/contracts/LandLord.sol
@@ -1,119 +1,124 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.10;
+// // SPDX-License-Identifier: MIT
+// pragma solidity 0.8.10;
 
-// aave
-import "@aave/contracts/misc/AaveProtocolDataProvider.sol";
-import "@aave/contracts/protocol/configuration/PoolAddressesProvider.sol";
+// // aave
+// import "@aave/contracts/misc/AaveProtocolDataProvider.sol";
+// import "@aave/contracts/protocol/configuration/PoolAddressesProvider.sol";
 
-// chainlink
-// import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
+// // chainlink
+// // import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 
-//openzeppelin
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+// //openzeppelin
+// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+// import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import "./ILendingSolution.sol";
+// import "./ILendingSolution.sol";
 
-// 1. Accept payment from tenant to landlord (payment screen)
-// 2. Write public view function to get balance of the contract (chainlink keeper)
-// 3. return a bool if youre the landlord (landlord dashboard page)
-// 4. Required: X amount of tenants
+// // 1. Accept payment from tenant to landlord (payment screen)
+// // 2. Write public view function to get balance of the contract (chainlink keeper)
+// // 3. return a bool if youre the landlord (landlord dashboard page)
+// // 4. Required: X amount of tenants
 
-contract LandLord {
+// contract LandLord {
 
-    address public landlord;
-    address public tenant;
-    uint256 public rent;
+//     address public landlord;
+//     address public tenant;
+//     uint256 public rent;
 
-    IERC20 public tokenUsedForPayments;
-    ILendingSolution public lendingService;
+//     IERC20 public tokenUsedForPayments;
+//     ILendingSolution public lendingService;
 
-    modifier onlyLandLord() {
-        require(msg.sender == landlord, "Restricted to the owner only");
-        _;
-    }
+//     modifier onlyLandLord() {
+//         require(msg.sender == landlord, "Restricted to the owner only");
+//         _;
+//     }
 
-    modifier onlyTenant() {
-        require(msg.sender == tenant, "Only tenant can access this");
-        _;
-    }
+//     modifier onlyTenant() {
+//         require(msg.sender == tenant, "Only tenant can access this");
+//         _;
+//     }
 
-    constructor(
-        address _landLord,
-        address _tenantAddress,
-        uint256 _rent,
-        address _tokenUsedToPay,
-        address _lendingService /*uint updateInterval*/
-    ) {
-        landlord = _landLord;
-        tenant = _tenantAddress;
-        rent = _rent;
-        tokenUsedForPayments = IERC20(_tokenUsedToPay);
-        lendingService = ILendingSolution(_lendingService);
-    }    
-    function approveTransfer() public onlyTenant {
-        tokenUsedForPayments.approve(address(landlord), rent);
+//     constructor(
+//         address _landLord,
+//         address _tenantAddress,
+//         uint256 _rent,
+//         address _tokenUsedToPay,
+//         address _lendingService /*uint updateInterval*/
+//     ) {
+//         landlord = _landLord;
+//         tenant = _tenantAddress;
+//         rent = _rent;
+//         tokenUsedForPayments = IERC20(_tokenUsedToPay);
+//         lendingService = ILendingSolution(_lendingService);
+//     }    
+//     function approveTransfer() public onlyTenant {
+//         tokenUsedForPayments.approve(address(landlord), rent);
 
-    }
+//     }
 
-    function transferToAave() public onlyTenant {
-        // require(tokenUsedForPayments.allowance(tenant, address(this)) >= rent, "No Allowance");
-        tokenUsedForPayments.transferFrom(tenant, address(this), rent);
-        // tokenUsedForPayments.approve(address(lendingService), rent);
-        // lendingService.depositFunds(rent);
-    }
+//     function transferToAave() public {
+//         // require(tokenUsedForPayments.allowance(tenant, address(this)) >= rent, "No Allowance");
+//         tokenUsedForPayments.transferFrom(tenant, address(lendingService), rent);
+        
+        
+//         tokenUsedForPayments.approve(address(aavePool), amount);
+//         // Deposit the amount in the LendingPool
+//         aavePool.deposit(address(tokenUsedForPayments), amount, address(this), 0);
+//         //tokenUsedForPayments.approve(address(lendingService), rent);
+//         lendingService.depositFunds(rent);
+//     }
 
 
-    // function addTenant(address _address) public onlyLandLord {
-    //     my_tenants[_address] = true;
-    // }
+//     // function addTenant(address _address) public onlyLandLord {
+//     //     my_tenants[_address] = true;
+//     // }
 
-    // function payRent() public payable onlyTenant {
-    //     //require(my_tenants[msg.sender], "You're not a tenant!");
-    //     tenants.push(payable(msg.sender));
-    // }
+//     // function payRent() public payable onlyTenant {
+//     //     //require(my_tenants[msg.sender], "You're not a tenant!");
+//     //     tenants.push(payable(msg.sender));
+//     // }
 
-    // function getBalance() public view returns (uint256) {
-    //     return address(this).balance;
-    // }
+//     // function getBalance() public view returns (uint256) {
+//     //     return address(this).balance;
+//     // }
 
-    // modifier: https://medium.com/coinmonks/solidity-tutorial-all-about-modifiers-a86cf81c14cb
-    // is used to change the behavior of a function in a declarative way
-    // function isOwner() public view virtual returns (bool) {
-    //     return msg.sender == landlord;
-    // }
+//     // modifier: https://medium.com/coinmonks/solidity-tutorial-all-about-modifiers-a86cf81c14cb
+//     // is used to change the behavior of a function in a declarative way
+//     // function isOwner() public view virtual returns (bool) {
+//     //     return msg.sender == landlord;
+//     // }
 
     
-}
-
-
-
-
-
-// function checkUpkeep(bytes calldata /*checkData */) external view override returns (bool upkeepNeeded, bytes memory performData) {
-//     upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
-//     return (upkeepNeeded, performData);
-// }
-// function performUpkeep(bytes calldata performData) external override {
-//     if ((block.timestamp - lastTimeStamp) > interval ) {
-//         lastTimeStamp = block.timestamp;
-//         counter = counter + 1;
-//     }
 // }
 
-// test
-// this is our stuff at the bottom
 
-// function checkUpkeep(bytes calldata /* checkData */) external view override returns (bool upkeepNeeded, bytes memory performData) {
-//     upkeepNeeded = address(this).balance > 2 ether; //2 * 1000000000000000000;
-//     performData = abi.encode(address(this));
-//     return (upkeepNeeded, performData);
-// }
 
-// function performUpkeep(bytes calldata performData) external override {
-//     if (address(this).balance > 3 ether ) {
-//         //transfer to aave
-//         //
-//         return 1;
-//     }
-// }
+
+
+// // function checkUpkeep(bytes calldata /*checkData */) external view override returns (bool upkeepNeeded, bytes memory performData) {
+// //     upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
+// //     return (upkeepNeeded, performData);
+// // }
+// // function performUpkeep(bytes calldata performData) external override {
+// //     if ((block.timestamp - lastTimeStamp) > interval ) {
+// //         lastTimeStamp = block.timestamp;
+// //         counter = counter + 1;
+// //     }
+// // }
+
+// // test
+// // this is our stuff at the bottom
+
+// // function checkUpkeep(bytes calldata /* checkData */) external view override returns (bool upkeepNeeded, bytes memory performData) {
+// //     upkeepNeeded = address(this).balance > 2 ether; //2 * 1000000000000000000;
+// //     performData = abi.encode(address(this));
+// //     return (upkeepNeeded, performData);
+// // }
+
+// // function performUpkeep(bytes calldata performData) external override {
+// //     if (address(this).balance > 3 ether ) {
+// //         //transfer to aave
+// //         //
+// //         return 1;
+// //     }
+// // }
